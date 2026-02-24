38d7c08e7e14b406969b101f72ddcdef1dee2849
Lawton Nichols <lawtonnichols@gmail.com>
2026-02-24 15:32:23 -0800

    .

---
+++ csci40-in-person/lec12/counter/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include "counter.h"
+using namespace std;
+
+void assertTrue(bool b, string description) {
+  if (b) {
+    cout << "PASSED: " << description << endl;
+  } else {
+    cout << "FAILED: " << description << endl;
+  }
+}
+
+void test_counter() {
+  init_counter();
+  inc();
+  inc();
+  inc();
+  dec();
+  assertTrue(get_count() == 2, "counter test");
+}
+
+int main() {
+  test_counter();
+
+  // init_counter();
+  // inc();
+  // inc();
+  // inc();
+  // dec();
+  // cout << get_count() << endl;
+
+  return 0;
+}
