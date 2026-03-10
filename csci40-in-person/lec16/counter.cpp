5da704ea3941131cb79e2fc941b415fce16a203b
Lawton Nichols <lawtonnichols@gmail.com>
2026-03-10 15:19:56 -0700

    .

--- csci40-in-person/lec16/counter.cpp
+++ csci40-in-person/lec16/counter.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 class Counter {
@@ -14,22 +15,25 @@ class Counter {
     void dec();
     int getCount() const;
 
+    Counter operator+(const Counter& other) const;
+    void operator++();
+
   private:
     int count;
 
-    vector<int> v;
-    const double d;
+    // vector<int> v;
+    // const double d;
 };
 
                 // initializer list 
                 // |
                 // v
-Counter::Counter() : count(0), v(100, 42), d(3.14) {
+Counter::Counter() : count(0) {
   cout << "I'm in the default constructor!\n";
   // count = 0;
 }
 
-Counter::Counter(int starting_count) : count(starting_count), d(5.67) {
+Counter::Counter(int starting_count) : count(starting_count) {
   cout << "I'm in the parameterized constructor!\n";
   // count = starting_count;
 }
@@ -56,6 +60,30 @@ int Counter::getCount() const {
   return count;
 }
 
+// option 1
+Counter Counter::operator+(const Counter& other) const {
+  Counter result;
+  result.count = count + other.count;
+  return result;
+}
+
+/*
+// option 2
+Counter operator+(const Counter& a, const Counter& b) {
+  Counter result(a.getCount() + b.getCount());
+  return result;
+}
+*/
+
+void Counter::operator++() {
+  // inc(); // this calls inc on the current object
+  count++;
+}
+
+bool operator<(const Counter& a, const Counter& b) {
+  return a.getCount() < b.getCount();
+}
+
 int main() {
   Counter c; // this line calls the default constructor
   // c.init(); // ==> Counter::init(c)
@@ -78,6 +106,27 @@ int main() {
   Counter d(33);
   d.inc();
   cout << d.getCount() << endl;
+  
+  /////////////////////////////////////
+
+  Counter sum = c + d; // C++ tries to call c.operator+(d) OR
+                       // operator+(c, d)
+  cout << sum.getCount() << endl;
+
+  // sum.inc();
+  ++sum; // sum.operator++() / operator++(sum)
+  cout << sum.getCount() << endl;
+
+  vector<Counter> v = {sum, c, d};
+  sort(v.begin(), v.end()); // sort will call operator<
+                            // c < d turns into either:
+                            //   - c.operator<(d), OR
+                            //   - operator<(c, d)
+
+  for (Counter ctr : v) {
+    cout << ctr.getCount() << " ";
+  }
+  cout << endl;
 
   return 0;
 }
