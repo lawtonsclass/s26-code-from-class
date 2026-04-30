6142a5d6a48889a008f097133c61add57c3c321e
Lawton Nichols <lawtonnichols@gmail.com>
2026-04-30 15:46:47 -0700

    .

---
+++ csci40-in-person/lec28/maps.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+
+int main() {
+  map<string, int> phonebook;
+  phonebook.emplace("Lawton", 5551234);
+  phonebook.emplace("Lonzo", 5551235);
+
+  cout << phonebook.count("Lawton") << endl;
+  cout << phonebook.count("Lawton's mom") << endl;
+
+  cout << phonebook.at("Lawton") << endl;
+  // cout << phonebook.at("Lawton's mom") << endl;
+
+  for (map<string, int>::iterator it = phonebook.begin(); 
+       it != phonebook.end(); 
+       it++) {
+    cout << (*it).first << " --> " << it->second << endl;
+  }
+
+  cout << endl;
+
+  for (pair<string, int> p : phonebook) {
+    cout << p.first << " --> " << p.second << endl;
+  }
+
+  cout << endl;
+
+  for (auto p : phonebook) {
+    cout << p.first << " --> " << p.second << endl;
+  }
+
+  return 0;
+}
