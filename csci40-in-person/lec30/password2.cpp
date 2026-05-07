3682d0acd77796bb8337f99401aa3945f94e7dee
Lawton Nichols <lawtonnichols@gmail.com>
2026-05-07 15:29:51 -0700

    .

---
+++ csci40-in-person/lec30/password2.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <cstring>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+  // _abC123
+  // xyzC456
+  if (argv[1][3] == 'C') {
+    printf("Access granted\n");
+  } else {
+    printf("Access denied\n");
+  }
+
+  return 0;
+}
